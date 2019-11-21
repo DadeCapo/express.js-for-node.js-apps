c544e5ec6b96be7bb58c991865c5cc603570a11b
Simon Choren <dadecapo@hotmail.com>
2019-11-21 00:47:16 -0500

    render html response and json response or a message to local host 3000 with nodemon activated

--- src/app.js
+++ src/app.js
@@ -4,25 +4,30 @@ const app = express()
 
 //app.com
 app.get('', (req, res) => {
-    res.send('Hello Capito')
+    res.send('<h1>Weather</h1>')
 })
 
 //app.com/help
 app.get('/help', (req, res) => {
-    res.send('Help Page')
+    res.send([{
+        name: 'Simon'
+    },{
+        name: 'Laura'
+    }])
 })
 
 //app.com/about
 app.get('/about', (req, res) => {
-    res.send('About Capito')
+    res.send('<h1>This is a COOL page!</h1>')
 })
 
 //app.com/weather
 app.get('/weather', (req, res) => {
-    res.send('Say it dont spray it')
+    res.send({
+        location: 'Miami',
+        forecast: 'All the types of weathers in one day!'
+    })
 })
-
-
 
 app.listen(3000, () => {
     console.log('Server is up on port 3000')
