6014addb46c9dc99b4b1dfc4cf23ffe1d2214299
Simon Choren <dadecapo@hotmail.com>
2019-12-12 02:08:04 -0500

    created fetch request to the endpoints location forecast and error when searching address forecast

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,13 @@
 console.log('Client side js file')
+
+fetch('http://localhost:3000/weather?address=miami').then((response) => {
+    response.json().then((data) => {
+        console.log(data)
+        if (data.error) {
+            console.log(data.error)
+        } else {
+            console.log(data.location)
+            console.log(data.forecast)
+        }
+    })
+})
